4b894c81c2e60775c066a6b0e4365573ccacbe33
probitie <viacheslav.mashkov@gmail.com>
2022-10-11 11:11:11 +0300

    my first regex

--- main.cpp
+++ main.cpp
@@ -7,36 +7,18 @@ using namespace std;
 
 int main()
 {
-    std::string s = "Some people, when confronted with a problem, think "
-        "\"I know, I'll use regular expressions.\" "
-        "Now they have two problems.";
+    regex exp(".o.");
+    smatch res;
 
-    std::regex self_regex("REGULAR EXPRESSIONS",
-            std::regex_constants::ECMAScript | std::regex_constants::icase);
-    if (std::regex_search(s, self_regex)) {
-        std::cout << "Text contains the phrase 'regular expressions'\n";
+    string str = "Some people, when confronted with a problem, think "
+                 "\"I know, I'll use regular expressions.\" "
+                 "Now they have two problems.";
+
+    string::const_iterator searchStart( str.cbegin() );
+    while ( regex_search( searchStart, str.cend(), res, exp ) )
+    {
+        cout << ( searchStart == str.cbegin() ? "" : " " ) << res[0];
+        searchStart = res.suffix().first;
     }
-
-    std::regex word_regex("(\\w+)");
-    auto words_begin =
-        std::sregex_iterator(s.begin(), s.end(), word_regex);
-    auto words_end = std::sregex_iterator();
-
-    std::cout << "Found "
-              << std::distance(words_begin, words_end)
-              << " words\n";
-
-    const int N = 6;
-    std::cout << "Words longer than " << N << " characters:\n";
-    for (std::sregex_iterator i = words_begin; i != words_end; ++i) {
-        std::smatch match = *i;
-        std::string match_str = match.str();
-        if (match_str.size() > N) {
-            std::cout << "  " << match_str << '\n';
-        }
-    }
-
-    std::regex long_word_regex("(\\w{7,})");
-    std::string new_s = std::regex_replace(s, long_word_regex, "[$&]");
-    std::cout << new_s << '\n';
+    cout << endl;
 }
